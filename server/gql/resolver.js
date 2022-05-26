864f628cea305be6f770b07dc845c6e3c02e71f3
Francisco Santiago <44105867+MasterFSC@users.noreply.github.com>
2022-05-26 14:28:12 -0500

    refactorizando funciones de resolver

--- server/gql/resolver.js
+++ server/gql/resolver.js
@@ -1,6 +1,4 @@
-const User = require("../models/user");
-const bcryptjs = require("bcryptjs")
-
+const userController = require("../controllers/user")
 const resolver = {
   Query: {
     // User
@@ -11,34 +9,7 @@ const resolver = {
   },
   Mutation: {
     //User
-    register: async (_, { input }) => {
-      console.log('registrando usuario'); 
-      const newUser = input;
-      newUser.email = newUser.email.toLowerCase();
-      newUser.username = newUser.username.toLowerCase();
-      
-      const { email, username, password } = newUser;
-
-      //Revisamos si el email esta en uso
-      const foundEmail = await User.findOne({ email });
-      if(foundEmail) throw new Error("El email ya está en uso");
-
-      //Revisamos si el username esta en uso
-      const foundUsername = await User.findOne({ username });
-      if(foundUsername) throw new Error("El username ya está en uso");
-
-      //Encriptar password
-      const salt = await bcryptjs.genSaltSync(10);
-      newUser.password = await bcryptjs.hash(password, salt)
-
-      try {
-        const user = new User(newUser);
-        user.save();
-        return user;
-      } catch (error) {
-        console.log('error', error)
-      }
-    }
+    register: async (_, { input }) => userController.register(input),
   },
 }
 
