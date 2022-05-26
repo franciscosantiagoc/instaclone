4d6f50a75cf9c1435ee079c186d6327c9370459f
Francisco Santiago <44105867+MasterFSC@users.noreply.github.com>
2022-05-26 00:59:42 -0500

    aregistrando usuario en la base de datos

--- server/gql/resolver.js
+++ server/gql/resolver.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 const resolver = {
   Query: {
     // User
@@ -8,10 +10,31 @@ const resolver = {
   },
   Mutation: {
     //User
-    register: (_, { input }) => {
+    register: async (_, { input }) => {
       console.log('registrando usuario'); 
-      console.log(input);
-      return null;
+      const newUser = input;
+      newUser.email = newUser.email.toLowerCase();
+      newUser.username = newUser.username.toLowerCase();
+      
+      const { email, username, password } = newUser;
+
+      //Revisamos si el email esta en uso
+      const foundEmail = await User.findOne({ email });
+      if(foundEmail) throw new Error("El email ya está en uso");
+
+      //Revisamos si el username esta en uso
+      const foundUsername = await User.findOne({ username });
+      if(foundUsername) throw new Error("El username ya está en uso");
+
+      //Encriptar password
+
+      try {
+        const user = new User(newUser);
+        user.save();
+        return user;
+      } catch (error) {
+        console.log('error', error)
+      }
     }
   },
 }
