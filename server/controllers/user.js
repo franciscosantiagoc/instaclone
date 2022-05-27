b4024fd03abbfd38fb670cd28f1c33afc29df83b
Francisco Santiago <44105867+MasterFSC@users.noreply.github.com>
2022-05-27 17:52:48 -0500

    Realizacion de funcion login y creacion del token de usuario

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,18 @@
 const User = require("../models/user");
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+function createToken(user, SECRET_KEY, expiresIn) {
+  const { id, name, email, username } = user;
+  const payload = {
+    id,
+    name,
+    email,
+    username
+  }
+
+  return jwt.sign(payload, SECRET_KEY, { expiresIn })
+}
 
 async function register(input) {
   console.log('registrando usuario'); 
@@ -32,8 +45,16 @@ async function register(input) {
 
 async function login(input) {
   const { email, password } = input;
-  console.log("Email: "+ email);
-  console.log("Password: "+password);
+  
+  const userFound = await User.findOne({ email: email.toLowerCase() });
+  if(!userFound) throw new Error("Error en el email o contraseña son incorrectos");
+
+  const passwordSuccess = await bcryptjs.compare(password, userFound.password)
+  if(!passwordSuccess) throw Error("Error en el email o contraseña son incorrectos");
+
+  return { 
+    token: createToken(userFound, process.env.SECRET_KEY, "24h") 
+  };
 }
 
 module.exports = {
