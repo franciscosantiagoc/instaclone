a46c03e745ff527408509ff99f53f05fa6b40bcb
Francisco Santiago <44105867+MasterFSC@users.noreply.github.com>
2022-05-28 01:51:59 -0500

    preparando cliente, instalando dependencias y configurando apollo provider

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { Button } from 'semantic-ui-react'
+import { ApolloProvider } from "@apollo/client"
+import client from "./config/apollo";
+
+export default function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="app">
+        <Button primary>Primary</Button>
+        <Button secondary>Secondary</Button>
+      </div>
+    </ApolloProvider>
+  );
+}
