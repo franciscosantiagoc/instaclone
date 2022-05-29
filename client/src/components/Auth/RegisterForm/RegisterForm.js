a0d705ab1854faa2ccf4d716850e8db35ed663d2
Francisco Santiago <44105867+MasterFSC@users.noreply.github.com>
2022-05-29 14:19:30 -0500

    Control dedatos del formulario de registro con formik

--- client/src/components/Auth/RegisterForm/RegisterForm.js
+++ client/src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,45 +1,68 @@
 import React from 'react'
 import { Form, Button } from 'semantic-ui-react';
+import  { useFormik } from 'formik';
 import './RegisterForm.scss';
 
 export default function RegisterForm(props) {
   const { setShowLogin } = props;
 
-  const onSubmit = () => {
-    console.log('Formulario Enviado')
-  }
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: null,
+    onSubmit: (formValue) => {
+      console.log("Form enviado")
+      console.log(formValue)
+    }
+  })
+
   return (
     <>
       <h2 className='register-form-title'>Registrate para conectar con tus amigos</h2>
-      <Form className='register-form' onSubmit={onSubmit}>
+      <Form className='register-form' onSubmit={formik.handleSubmit}>
         <Form.Input 
         type='text'
         placeholder='Nombre y apellidos'
-        name='name'/>
+        name='name'
+        onChange={formik.handleChange}/>
 
       <Form.Input 
         type='text'
         placeholder='Nombre de usuario'
-        name='username'/> 
+        name='username'
+        onChange={formik.handleChange}/> 
 
       <Form.Input 
         type='text'
         placeholder='Correo electronico'
-        name='username'/> 
+        name='email'
+        onChange={formik.handleChange}/> 
       
       <Form.Input 
         type='password'
         placeholder='Contraseña'
-        name='password'/>
+        name='password'
+        onChange={formik.handleChange}/>
 
       <Form.Input 
         type='password'
         placeholder='Repetir contraseña'
-        name='repeat-password'/>     
+        name='repeatPassword'
+        onChange={formik.handleChange}/>     
 
       <Button className='btn-submit' type='submit'>Registrarse</Button>   
 
       </Form>     
     </>
   )
+}
+
+
+function initialValues() {
+  return {
+    name: '',
+    username: '',
+    email: '',
+    password: '',
+    repeatPassword: ''
+  }
 }
