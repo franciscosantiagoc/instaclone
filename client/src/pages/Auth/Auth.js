079230b6e2a87d4911fd9bc03d6638e1edb8cc5f
Francisco Santiago <44105867+MasterFSC@users.noreply.github.com>
2022-05-29 13:49:34 -0500

    Creacion de formulario de registro

--- client/src/pages/Auth/Auth.js
+++ client/src/pages/Auth/Auth.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Container, Image } from 'semantic-ui-react';
+import RegisterForm from '../../components/Auth/RegisterForm';
+import instaclone from '../../assets/img/instaclone.png'
 import "./Auth.scss";
 
 export default function Auth() {
+  const [showLogin, setShowLogin] = useState(false);
   return (
-    <h1>Estamos en Auth</h1>
+    <Container fluid className="auth">
+      <Image src={instaclone} />
+
+      <div className='container-form'>
+        {
+          showLogin ? <p>formulario login </p> : <RegisterForm setShowLogin={setShowLogin} />
+        }
+      </div>
+
+      <div className='change-form'>
+        <p>
+        {
+          showLogin ? (
+            <>
+              ¿No estas registrado?
+              <span onClick={() => setShowLogin(!showLogin)}>Registrate</span>
+            </>
+          ) : (
+            <>
+              Entrar a tu cuenta
+              <span onClick={() => setShowLogin(!showLogin)}>Iniciar sesión</span>
+            </>
+          )
+        }
+        </p>
+      </div>
+    </Container>
   )
 }
