6c44d07d1b3b4d68123bac5581f7d034597d823a
=Francisco Santiago <=fcosantiagoc@gmail.com>
2022-10-09 18:13:06 -0500

    Mostrando el avatar en el perfil del usuario

--- client/src/components/User/AvatarForm/AvatarForm.jsx
+++ client/src/components/User/AvatarForm/AvatarForm.jsx
@@ -1,54 +1,79 @@
-import React, { useCallback, useState } from 'react';
-import { Button } from 'semantic-ui-react';
-import { useDropzone } from 'react-dropzone';
-import { toast } from 'react-toastify';
-import { useMutation } from '@apollo/client';
-import { UPDATE_AVATAR } from '../../../gql/user';
-import './AvatarForm.scss';
+import React, { useCallback, useState } from "react";
+import { Button } from "semantic-ui-react";
+import { useDropzone } from "react-dropzone";
+import { toast } from "react-toastify";
+import { useMutation } from "@apollo/client";
+import { UPDATE_AVATAR, GET_USER } from "../../../gql/user";
+import "./AvatarForm.scss";
 
 export default function AvatarForm(props) {
-  const { setShowModal } = props;
+  const { setShowModal, auth } = props;
   const [loading, setLoading] = useState(false);
 
-  const [updateAvatar] = useMutation(UPDATE_AVATAR);
+  const [updateAvatar] = useMutation(UPDATE_AVATAR, {
+    update(cache, { data: { updateAvatar } }) {
+      /* console.log('updateAvatar', updateAvatar) */
+      const { getUser } = cache.readQuery({
+        query: GET_USER,
+        variables: {
+          username: auth.username,
+        },
+      });
 
-  const onDrop = useCallback(async (acceptedFile)=>{
+      cache.writeQuery({
+        query: GET_USER,
+        variables: {
+          username: auth.username,
+        },
+        data: {
+          getUser: {
+            ...getUser,
+            avatar: updateAvatar.urlAvatar,
+          },
+        },
+      });
+    },
+  });
+
+  const onDrop = useCallback(async (acceptedFile) => {
     const file = acceptedFile[0];
 
     try {
       setLoading(true);
-      const result = await updateAvatar({ variables: { file }});
+      const result = await updateAvatar({ variables: { file } });
       const { data } = result;
-      if(!data.updateAvatar.status){
+      if (!data.updateAvatar.status) {
         toast.error("Error al actualizar el avatar");
         setLoading(false);
-      }else {
+      } else {
         setLoading(false);
         setShowModal(false);
       }
-     /*  console.log(result);       */
+      /*  console.log(result);       */
     } catch (error) {
       console.log(error);
     }
-  }, [])
+  }, []);
 
   const { getRootProps, getInputProps } = useDropzone({
     /* accept: ["image/jpeg", "image/png"], */
     accept: {
-      'image/png': ['.png'],
-      'image/jpeg': ['.jpeg', '.jpg'],
+      "image/png": [".png"],
+      "image/jpeg": [".jpeg", ".jpg"],
     },
     noKeyboard: true,
     multiple: false,
     onDrop,
-  })
+  });
 
   return (
-    <div className='avatar-form'>
-      <Button {...getRootProps()} loading={loading}>Cargar una foto</Button>
+    <div className="avatar-form">
+      <Button {...getRootProps()} loading={loading}>
+        Cargar una foto
+      </Button>
       <Button>Eliminar foto actual</Button>
-      <Button onClick={ () => setShowModal(false)}>Cancelar</Button>
+      <Button onClick={() => setShowModal(false)}>Cancelar</Button>
       <input {...getInputProps()} />
     </div>
-  )
+  );
 }
--- client/src/components/User/Profile/Profile.jsx
+++ client/src/components/User/Profile/Profile.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import { Grid, Image } from 'semantic-ui-react';
-import { useQuery } from '@apollo/client';
-import { GET_USER } from '../../../gql/user';
-import userAuth from '../../../hooks/useAuth';
-import UserNotFound from '../../UserNotFound/UserNotFound';
-import ModalBasic from '../../Modal/ModalBasic/ModalBasic';
-import AvatarForm from '../AvatarForm/AvatarForm';
-import ImageNotFound from '../../../assets/img/avatar.png';
-import './Profile.scss';
+import React, { useState } from "react";
+import { Grid, Image } from "semantic-ui-react";
+import { useQuery } from "@apollo/client";
+import { GET_USER } from "../../../gql/user";
+import userAuth from "../../../hooks/useAuth";
+import UserNotFound from "../../UserNotFound/UserNotFound";
+import ModalBasic from "../../Modal/ModalBasic/ModalBasic";
+import AvatarForm from "../AvatarForm/AvatarForm";
+import ImageNotFound from "../../../assets/img/avatar.png";
+import "./Profile.scss";
 
 export default function Profile(props) {
   const { username } = props;
   const [showModal, setShowModal] = useState(false);
-  const [titleModal, setTitleModal] = useState('');
+  const [titleModal, setTitleModal] = useState("");
   const [childrenModal, setChildrenModal] = useState(null);
 
   const { auth } = userAuth();
@@ -21,42 +21,52 @@ export default function Profile(props) {
     variables: { username },
   });
 
-  if(loading) return null;
-  if(error) return <UserNotFound />
+  if (loading) return null;
+  if (error) return <UserNotFound />;
 
   const { getUser } = data;
 
   const handlerModal = (type) => {
     switch (type) {
-      case 'avatar':
-        setTitleModal('Cambiar foto de perfil');
-        setChildrenModal(<AvatarForm setShowModal = {setShowModal}/>)
+      case "avatar":
+        setTitleModal("Cambiar foto de perfil");
+        setChildrenModal(
+          <AvatarForm setShowModal={setShowModal} auth={auth} />
+        );
         setShowModal(true);
         break;
       default:
         break;
     }
-  }
+  };
 
   return (
     <>
-      <Grid className='profile'>
-        <Grid.Column width={5} className='profile__left'>
-          <Image src={ImageNotFound} avatar 
-          onClick={ ()=> username === auth.username && handlerModal('avatar') } />
+      <Grid className="profile">
+        <Grid.Column width={5} className="profile__left">
+          <Image
+            src={getUser.avatar ? getUser.avatar : ImageNotFound}
+            avatar
+            onClick={() => username === auth.username && handlerModal("avatar")}
+          />
         </Grid.Column>
-        <Grid.Column width={11} className='profile__right'>
+        <Grid.Column width={11} className="profile__right">
           <div>Header profile</div>
           <div>Followers</div>
-          <div className='other'>
-            <p className='name'>{getUser.name}</p>
+          <div className="other">
+            <p className="name">{getUser.name}</p>
             {getUser.siteWeb && (
-              <a href={getUser.siteWeb} className='siteWeb' target='_blank' rel="noreferrer">
+              <a
+                href={getUser.siteWeb}
+                className="siteWeb"
+                target="_blank"
+                rel="noreferrer"
+              >
                 {getUser.siteWeb}
               </a>
             )}
             {getUser.description && (
-              <p className='description'>{getUser.description}</p>
+              <p className="description">{getUser.description}</p>
             )}
           </div>
         </Grid.Column>
@@ -65,5 +75,5 @@ export default function Profile(props) {
         {childrenModal}
       </ModalBasic>
     </>
-  )
+  );
 }
