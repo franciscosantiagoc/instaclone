e51c5f30f71c9814514f5ad257b71f62021977c8
=Francisco Santiago <=fcosantiagoc@gmail.com>
2022-10-09 00:38:01 -0500

    Enviando token del cliente en peticiones al server

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -9,60 +9,62 @@ function createToken(user, SECRET_KEY, expiresIn) {
     id,
     name,
     email,
-    username
-  }
+    username,
+  };
 
-  return jwt.sign(payload, SECRET_KEY, { expiresIn })
+  return jwt.sign(payload, SECRET_KEY, { expiresIn });
 }
 
 async function register(input) {
-  console.log('registrando usuario'); 
-      const newUser = input;
-      newUser.email = newUser.email.toLowerCase();
-      newUser.username = newUser.username.toLowerCase();
-      
-      const { email, username, password } = newUser;
+  console.log("registrando usuario");
+  const newUser = input;
+  newUser.email = newUser.email.toLowerCase();
+  newUser.username = newUser.username.toLowerCase();
 
-      //Revisamos si el email esta en uso
-      const foundEmail = await User.findOne({ email });
-      if(foundEmail) throw new Error("El email ya está en uso");
+  const { email, username, password } = newUser;
 
-      //Revisamos si el username esta en uso
-      const foundUsername = await User.findOne({ username });
-      if(foundUsername) throw new Error("El username ya está en uso");
+  //Revisamos si el email esta en uso
+  const foundEmail = await User.findOne({ email });
+  if (foundEmail) throw new Error("El email ya está en uso");
 
-      //Encriptar password
-      const salt = await bcryptjs.genSaltSync(10);
-      newUser.password = await bcryptjs.hash(password, salt)
+  //Revisamos si el username esta en uso
+  const foundUsername = await User.findOne({ username });
+  if (foundUsername) throw new Error("El username ya está en uso");
 
-      try {
-        const user = new User(newUser);
-        user.save();
-        return user;
-      } catch (error) {
-        console.log('error', error)
-      }
+  //Encriptar password
+  const salt = await bcryptjs.genSaltSync(10);
+  newUser.password = await bcryptjs.hash(password, salt);
+
+  try {
+    const user = new User(newUser);
+    user.save();
+    return user;
+  } catch (error) {
+    console.log("error", error);
+  }
 }
 
 async function login(input) {
   const { email, password } = input;
-  
+
   const userFound = await User.findOne({ email: email.toLowerCase() });
-  if(!userFound) throw new Error("Error en el email o contraseña son incorrectos");
+  if (!userFound)
+    throw new Error("Error en el email o contraseña son incorrectos");
 
-  const passwordSuccess = await bcryptjs.compare(password, userFound.password)
-  if(!passwordSuccess) throw Error("Error en el email o contraseña son incorrectos");
+  const passwordSuccess = await bcryptjs.compare(password, userFound.password);
+  if (!passwordSuccess)
+    throw Error("Error en el email o contraseña son incorrectos");
 
-  return { 
-    token: createToken(userFound, process.env.SECRET_KEY, "24h") 
+  return {
+    token: createToken(userFound, process.env.SECRET_KEY, "24h"),
   };
 }
 
 async function getUser(id, username) {
   let user = null;
-  if(id) user = await User.findById(id);
-  if(username) user = await User.findOne({ username });
-  if(!user) throw new Error("El usuario no existe");
+  if (id) user = await User.findById(id);
+  if (username) user = await User.findOne({ username });
+  if (!user) throw new Error("El usuario no existe");
 
   return user;
 }
@@ -74,13 +76,15 @@ async function updateAvatar(file, ctx) {
   const fileData = createReadStream(imageName);
   try {
     const result = await awsUploadImage(fileData, imageName);
-    console.log('result', result)
+    console.log("result", result);
   } catch (error) {
     return {
       status: false,
       urlAvatar: null,
-    }
+    };
   }
+  /* console.log("Ejecutando updateAvatar")
+  console.log('ctx', ctx) */
 
   return null;
 }
@@ -89,4 +93,4 @@ module.exports = {
   login,
   getUser,
   updateAvatar,
-}
+};
