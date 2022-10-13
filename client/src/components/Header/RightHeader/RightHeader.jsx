8aa10a34009ceb8fc43ae0bd80976b4b104343c7
=Francisco Santiago <=fcosantiagoc@gmail.com>
2022-10-12 22:17:47 -0500

    Actualizando el avatar del menu

--- client/src/components/Header/RightHeader/RightHeader.jsx
+++ client/src/components/Header/RightHeader/RightHeader.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Icon, Image} from 'semantic-ui-react'
 import { Link } from 'react-router-dom';
-import useAuth from '../../../hooks/useAuth';
-import ImageNotFound from '../../../assets/img/avatar.png'
+import { useQuery } from "@apollo/client";
+import { GET_USER } from "../../../gql/user";
+import useAuth from "../../../hooks/useAuth";
+import ImageNotFound from "../../../assets/img/avatar.png";
 import './RightHeader.scss';
 
 export default function RightHeader() {
   const { auth } = useAuth();
+  const { data, loading, error } = useQuery(GET_USER, {
+    variables: { username: auth.username }
+  });
+
+  if(loading || error) return null;
+  const { getUser } = data;
   
   return (
     <div className='right-header'>
@@ -15,7 +23,7 @@ export default function RightHeader() {
       </Link>
       <Icon name='plus' />
       <Link to={`/${auth.username}`}>
-        <Image src={ImageNotFound} avatar/>
+        <Image src={getUser.avatar? getUser.avatar : ImageNotFound} avatar/>
       </Link>
       
     </div>
